693ab0e5a91c414fb1fffe2eabc2b18e532df54a
swatjoker <iswatxjokeri@gmail.com>
2020-08-25 17:52:40 -0400

    not bad

--- W7D2/MovieApp/app/controllers/application_controller.rb
+++ W7D2/MovieApp/app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :login!, :logout!
 
     def current_user
+        return nil unless session[:session_token]
         @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_user
     end
 
     def login!(user)
